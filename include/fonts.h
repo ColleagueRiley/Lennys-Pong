3d44d4e8946505eb52507d33b859be8aca077b50
Colleague Riley <ColleagueRiley@gmail.com>
2023-07-29 15:32:06 -0400

    added font system

---
+++ include/fonts.h
@@ -0,0 +1,42 @@
+const char* font_ttfs[] = {    
+    "DejaVuSans.ttf",
+    "LiberationSans-Regular.ttf",
+    "DroidSans.ttf",
+    "Roboto-Regular.ttf",
+    "Ubuntu-Regular.ttf",
+    "NotoSans-Regular.ttf",
+    "NotoSansNerdFontPropo-Regular.ttf",
+
+    "/usr/share/fonts/TTF/DejaVuSans.ttf",
+    "/usr/share/fonts/TTF/LiberationSans-Regular.ttf",
+    "/usr/share/fonts/TTF/DroidSans.ttf",
+    "/usr/share/fonts/TTF/Roboto-Regular.ttf",
+    "/usr/share/fonts/TTF/Ubuntu-Regular.ttf",
+    "/usr/share/fonts/TTF/NotoSans-Regular.ttf",
+    "/usr/share/fonts/TTF/NotoSansNerdFontPropo-Regular.ttf",
+
+    "~/.fonts/DejaVuSans.ttf",
+    "~/.fonts/LiberationSans-Regular.ttf",
+    "~/.fonts/DroidSans.ttf",
+    "~/.fonts/Roboto-Regular.ttf",
+    "~/.fonts/Ubuntu-Regular.ttf",
+    "~/.fonts/NotoSans-Regular.ttf",
+    "~/.fonts/NotoSansNerdFontPropo-Regular.ttf"
+};
+
+#ifdef __unix__
+#include <sys/stat.h>
+#endif
+
+inline bool si_path_exists(char* path);
+bool si_path_exists(char* path) {
+	#if defined(SI_SYSTEM_WINDOWS)
+		DWORD file_attrib = GetFileAttributes(path);
+		return file_attrib != INVALID_FILE_ATTRIBUTES;
+	#else
+
+		struct stat tmp;
+		return (stat(path, &tmp) == 0);
+
+	#endif
+}
